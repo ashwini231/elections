fb8cf15e7ab3ef254e621f31a3aa694170731214
ashwini231 <65889407+ashwini231@users.noreply.github.com>
2022-10-31 22:42:26 +0530

    Add files via upload

--- Election.sol
+++ Election.sol
@@ -26,7 +26,8 @@ contract Election {
         string email;
         bool verified;
         bool registered;
-        bool voted;
+        uint voted;
+        string voteDate;
     }
     // Store accounts that have voted
     mapping(address => bool) public voters;
@@ -39,16 +40,24 @@ contract Election {
     uint public constituencyCount;
     uint public candidatesCount;
     uint public voterListCount;
+
+    string public startdate;
+    string public enddate;
+  
+
     // voted event
     event votedEvent (
         uint indexed _candidateId
     );
 
     constructor () {
-        addVoterList("1111", "Ashwini", "23/04/2001", "2222", "101");
+        addVoterList("YCV0164822", "Ashwini M S", "23/04/2001", "390051307206", "101");
+        addVoterList("YBT0674825", "Thrishya R", "26/07/1999", "870051407316", "101");
+        addVoterList("GRY0878526", "Monika R", "13/11/2002", "407316870051", "102");
     }
     
     function addCandidate (string memory _name, string memory _partyname, string memory _constituency) public {
+        
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name,_partyname, _constituency,0);
     }
@@ -58,14 +67,13 @@ contract Election {
     }
     function addVoterList (string memory _voterID, string memory _name, string memory _dob,string memory _aadhar,string memory _constituency) public {
         voterListCount ++;
-        voterlist[_voterID] = VoterList(_voterID,_name,_dob,_aadhar,_constituency,"","","",false,false,false);
+        voterlist[_voterID] = VoterList(_voterID,_name,_dob,_aadhar,_constituency,"","","",false,false,0,"");
     }
-    function verifyVoter(string memory _voterID1, string memory _name1, string memory _dob1,string memory _aadhar1,string memory _constituency1) public{
+    function verifyVoter(string memory _voterID1, string memory _name1, string memory _dob1,string memory _aadhar1) public{
         require(keccak256(abi.encodePacked(voterlist[_voterID1].voterID))==keccak256(abi.encodePacked(_voterID1)));
         require(keccak256(abi.encodePacked(voterlist[_voterID1].name))==keccak256(abi.encodePacked(_name1)));
         require(keccak256(abi.encodePacked(voterlist[_voterID1].dob))==keccak256(abi.encodePacked(_dob1)));
         require(keccak256(abi.encodePacked(voterlist[_voterID1].aadhar))==keccak256(abi.encodePacked(_aadhar1)));
-        require(keccak256(abi.encodePacked(voterlist[_voterID1].constituency))==keccak256(abi.encodePacked(_constituency1)));
         require(voterlist[_voterID1].verified==false);
         voterlist[_voterID1].verified=true;
     }
@@ -82,24 +90,29 @@ contract Election {
         voterlist[_voterid].registered=true;
     }
 
-    function voterLogin(string memory _voterID, string memory _password,string memory _constituency) public{
+    function voterLogin(string memory _voterID, string memory _password) public{
         require(keccak256(abi.encodePacked(voterlist[_voterID].voterID))==keccak256(abi.encodePacked(_voterID)));
         require(keccak256(abi.encodePacked(voterlist[_voterID].password))==keccak256(abi.encodePacked(_password)));  
-        require(keccak256(abi.encodePacked(voterlist[_voterID].constituency))==keccak256(abi.encodePacked(_constituency)));        
         require(voterlist[_voterID].verified==true);
         require(voterlist[_voterID].registered==true);
     }
 
-    function vote (uint _candidateId) public {
+    function setDate(string memory _startDate, string memory _endDate) public{
+        startdate=_startDate;
+        enddate=_endDate;
+    }
+    function vote (uint _candidateId,string memory _voterid,string memory _date) public {
         // require that they haven't voted before
         require(!voters[msg.sender]);
 
+        require(voterlist[_voterid].voted==0);
         // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
         // record that voter has voted
         voters[msg.sender] = true;
-
+        voterlist[_voterid].voted=1;
+        voterlist[_voterid].voteDate=_date;
         // update candidate vote Count
         candidates[_candidateId].voteCount ++;
 
